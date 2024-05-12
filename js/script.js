a0722e1e76c4de1d793d1b698a685fe51ba97d02
mahmudgalal <93224400+mahmudgalal@users.noreply.github.com>
2024-05-12 20:41:40 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".moveletter", {
+    strings: ["Designer" , "Larry Daniels","Developer" ],
+    loop: true,
+    typeSpeed: 35,
+    backSpeed: 15,
+    backDelay: 500,
+});
